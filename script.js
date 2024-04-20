d438dee528eec93f42eff8347cfa514bb667009c
khoaBK96 <khoaBK96@gmail.com>
2024-04-20 12:03:52 +0700

    complete style for button

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 const myLibrary = []
-
+let bookCount = 1;
+const removes = []
 function Book(title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read == 'read' ? 'Read' : 'Not read'
+    this.bookCount = bookCount
 }
 
 function displayBooks(books){
     let html = ''
     books.forEach(book => {
-        html += `<div class='card'>
+        html += `<div class='card' data-attribute=${book.bookCount}>
             <h3>${book.title}</h3>
             <h3>${book.author}</h3>
             <h3>${book.pages}</h3>
-            <button>${book.read}</button>
-            <button>Remove</button>
+            <button id='readBtn' class='btn'>${book.read}</button>
+            <button id = 'remove${book.bookCount}' class='btn'>Remove</button>
         </div>`
     });
     document.querySelector('.book-grid').innerHTML= html
+   
 }
 
 
-const addBookBtn = document.querySelector('.addBookBtn')
+const addBookBtn = document.querySelector('.addBook')
 const bookDialog = document.querySelector('.bookDialog')
 const submit = document.getElementById('confirmBtn')
 const title = document.getElementById('title')
@@ -35,13 +38,15 @@ addBookBtn.addEventListener('click', () => bookDialog.showModal())
 
 submit.addEventListener('click', e => {
     if(title.value != ''&& author.value != '' && pages.value !='' && read.value !=''){
-    let book = new Book(title.value, author.value, pages.value, read.value)
+    let book = new Book(title.value, author.value, pages.value, read.value, bookCount)
     myLibrary.push(book)
     displayBooks(myLibrary)
     title.value=''
     author.value=''
     pages.value=''
-    }
+    bookCount += 1
+}
+    
 })
 
 cancel.addEventListener('click', () => {
@@ -52,3 +57,4 @@ read.value=''
 bookDialog.close()
 }
 )
+
